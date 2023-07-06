52450a40fef997705d334a253b28b39b3fbb4b26
Rinya7 <v.dmitriev0712@gmail.com>
2023-07-06 21:04:10 +0200

    task_3_1_to_check

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,15 +7,24 @@ import { ContactList } from '../ContactList/ContactList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = JSON.parse(localStorage.getItem('thisStat'));
+    if (localContacts)
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('thisStat')),
+      });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('thisStat', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleContactSubmits = ({ name, number }) => {
     const simpleContact = this.state.contacts.find(
